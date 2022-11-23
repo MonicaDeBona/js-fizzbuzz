6d53a635a4fd1423124763e7207a303302874fc5
MonicaDeBona <debona.monica@outlook.it>
2022-11-23 18:10:47 +0100

    Update append

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
 
 for (let i = 1; i < 101; i++) { //chiedo al programma di mostrare in console i numeri da uno a 100
     
-
+    let container = document.querySelector('.container')
     let div = document.createElement("div")
     
 
     if (i%3 === 0 && i%5 === 0) {
         console.log("FizzBuzz");
-        div.append(i)
+        div.append('FizzBuzz')
     } else if (i%3 === 0) {
         console.log("Fizz");
-
+        div.append('Fizz')
     } else if (i%5 === 0 ) {                      
         console.log("Buzz");
-
+        div.append('Buzz')
     } else {
         console.log(i);
     }
 
+    container.append(div)
 
-
-    
-    console.log(div.textContent) 
     
 }
 
